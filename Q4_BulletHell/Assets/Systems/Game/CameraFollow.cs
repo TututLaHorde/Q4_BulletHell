32db00be40ed6c397da815673efc02ddf28be3c4
TututLaHorde <a.boucher@lahorde.school>
2023-11-30 17:13:00 +0100

    BossEnemiesDistinction
    Win when all BOSSES are dead
    Camera follow the last boss death

--- Q4_BulletHell/Assets/Systems/Game/CameraFollow.cs
+++ Q4_BulletHell/Assets/Systems/Game/CameraFollow.cs
@@ -4,18 +4,29 @@ namespace BH.Game
 {
     public class CameraFollow : MonoBehaviour
     {
-        [SerializeField] private Transform trPlayer;
+        public Transform trsTarget;
+        private Transform trsInitial;
         [SerializeField] private float smoothTime;
         private Vector3 velocity = Vector3.zero;
 
+        private void Start()
+        {
+            trsInitial = trsTarget;
+        }
+
         void FixedUpdate()
         {
-            if (trPlayer != null)
+            if (trsTarget != null)
             {
                 //follow player with smooth
-                Vector3 targetPosition = new Vector3(trPlayer.position.x, trPlayer.position.y, transform.position.z);
+                Vector3 targetPosition = new Vector3(trsTarget.position.x, trsTarget.position.y, transform.position.z);
                 transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
             }
+        }
+
+        public void ResetTarget()
+        {
+            trsTarget = trsInitial;
         }
     }
 }
